61f6c90e2728a2c93a6812576f500f46b97f2709
CuDuyDuc <duccu1403@gmail.com>
2024-03-16 02:32:15 +0700

    Update

---
+++ src/component/index.ts
@@ -0,0 +1,17 @@
+import ButtonComponent from "./ButtonComponent";
+import ContainerComponent from "./ContainerComponent";
+import InputComponent from "./InputComponent";
+import KeyboardAvoidingWrapper from "./KeyboardAvoidingWrapper";
+import RowComponent from "./RowComponent";
+import SectionComponent from "./SectionComponent";
+import TextComponent from "./TextComponent";
+
+export {
+    TextComponent, 
+    ButtonComponent, 
+    InputComponent, 
+    SectionComponent,
+    KeyboardAvoidingWrapper,
+    RowComponent,
+    ContainerComponent,
+};
